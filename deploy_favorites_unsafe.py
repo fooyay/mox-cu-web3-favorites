63571a30fb0e4ec2136eb82c91b3e424c653918e
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-06 10:48:51 -0500

    get private key from env (unsafe version)

--- deploy_favorites_unsafe.py
+++ deploy_favorites_unsafe.py
@@ -1,5 +1,12 @@
 from vyper import compile_code
 from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+RPC_URL = os.getenv("RPC_URL")
+MY_ADDRESS = os.getenv("MY_ADDRESS")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 
 
 def main():
@@ -9,7 +16,7 @@ def main():
         compilation_details = compile_code(favorites_code, output_formats=["bytecode", "abi"])
         # print(compilation_details)
 
-    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+    w3 = Web3(Web3.HTTPProvider(RPC_URL))
 
     favorites_contract = w3.eth.contract(bytecode=compilation_details["bytecode"], abi=compilation_details["abi"])
     # print(favorites_contract)
@@ -24,7 +31,8 @@ def main():
         }
     )
 
-    w3.eth.account.sign_transaction(transaction, private_key=private_key)
+    signed_transaction = w3.eth.account.sign_transaction(transaction, private_key=PRIVATE_KEY)
+    print(signed_transaction)
 
 
 if __name__ == "__main__":
